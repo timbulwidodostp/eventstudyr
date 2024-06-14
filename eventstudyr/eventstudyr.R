410d04e083f57dc5610176fc136ef0f1b673bf82
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-06-14 14:25:25 +0700

    Use eventstudyr With (In) R Software

--- eventstudyr/eventstudyr.R
+++ eventstudyr/eventstudyr.R
@@ -1,20 +1,17 @@
 # Olah Data Semarang
 # WhatsApp : +6285227746673
 # IG : @olahdatasemarang_
-# Estimation (Estimate) and Visualization (visualize) in the Linear Panel Event Study Design models Use eventstudyr With (In) Software
+# Estimation (Estimate) and Visualization (visualize) in the Linear Panel Event Study Design models Use eventstudyr With (In) R Software
 install.packages("eventstudyr")
 library("eventstudyr")
 eventstudyr = read.csv("https://raw.githubusercontent.com/timbulwidodostp/eventstudyr/main/eventstudyr/eventstudyr.csv",sep = ";")
-# Estimation (Estimate) and Visualization (visualize) in the Linear Panel Event Study Design models Use eventstudyr With (In) Software
+# Estimation (Estimate) and Visualization (visualize) in the Linear Panel Event Study Design models Use eventstudyr With (In) R Software
 eventstudyr<-EventStudy(estimator="OLS",data=eventstudyr,outcomevar="y_smooth_m",policyvar="z",idvar="id",timevar="t",controls="x_r",pre=0,post=4)
 summary(eventstudyr$output)
 eventstudyr <- EventStudyPlot(estimates = eventstudyr)
 eventstudyr
-# Estimation (Estimate) and Visualization (visualize) in the Linear Panel Event Study Design models Use eventstudyr With (In) Software
+# Estimation (Estimate) and Visualization (visualize) in the Linear Panel Event Study Design models Use eventstudyr With (In) R Software
 # Olah Data Semarang
 # WhatsApp : +6285227746673
 # IG : @olahdatasemarang_
 # Finished
-
-
-
